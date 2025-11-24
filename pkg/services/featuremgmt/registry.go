6c41238e64912c98cd5b852c37bff66552a5f438
tomerqodo <tomer.y@qodo.ai>
2025-11-24 09:41:52 +0200

    Apply changes for benchmark PR

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -2107,12 +2107,6 @@ var (
 			FrontendOnly: false,
 		},
 		{
-			Name:        "grafanaAdvisorAppInstaller",
-			Description: "Enables Advisor app installer",
-			Stage:       FeatureStageExperimental,
-			Owner:       grafanaPluginsPlatformSquad,
-		},
-		{
 			Name:        "kubernetesAnnotations",
 			Description: "Enables app platform API for annotations",
 			Stage:       FeatureStageExperimental,
