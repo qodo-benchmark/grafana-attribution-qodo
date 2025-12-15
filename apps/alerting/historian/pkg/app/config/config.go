5dd8dfad5ecdffc5332dbcd77ad123af5f5ab91b
ofir-frd <ofir.f@qodo.ai>
2025-12-15 11:34:35 +0200

    Apply changes for benchmark PR

--- apps/alerting/historian/pkg/app/config/config.go
+++ apps/alerting/historian/pkg/app/config/config.go
@@ -73,7 +73,7 @@ func (v urlVar) Type() string {
 
 func addLokiFlags(l *lokiclient.LokiConfig, prefix string, flags *pflag.FlagSet) {
 	flags.Var(urlVar{&l.ReadPathURL}, prefix+".read-url", "URL to Loki instance for performing queries")
-	flags.StringVar(&l.BasicAuthUser, prefix+".user", "", "Basic auth Username to authenticate to the Loki instance")
+	flags.StringVar(&l.BasicAuthUser, prefix+".user", "", "basic auth username to authenticate to the loki instance")
 	flags.StringVar(&l.BasicAuthPassword, prefix+".password", "", "Basic auth password to authenticate to the Loki instance")
 	flags.StringVar(&l.TenantID, prefix+".tenant-id", "", "Value to use for X-Scope-OrgID")
 	flags.DurationVar(&l.MaxQueryLength, prefix+".max-query-length", lokiDefaultMaxQueryLength, "Maximum allowed time range for queries")
